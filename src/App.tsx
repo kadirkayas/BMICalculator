ed15ca3b88000c4a319c3e1e2c4d5292fb9c55d9
cmwkadir <kadirkayas2208@gmail.com>
2024-05-20 10:16:25 +0900

    finish

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import "./App.css"
+
+function App() {
+  const[bmi, setBmi] = useState(0)
+  const[rate, setRate] = useState('')
+  function calculateBmi(height: number, weight: number) {
+    const bmiV=weight / (height * height)
+    setBmi(bmiV)
+    console.log(bmiV);
+    if (bmiV < 18.5) {
+      setRate("Underweight")
+    } else if (bmiV < 25) {
+      setRate("Normal")
+    } else if (bmiV < 30) {
+      setRate("Overweight")
+    } else {
+      setRate("Obese")
+    }
+  }
+  function handleSubmit(e:any) {
+    e.preventDefault()
+    const height = e.target[0].value/100
+    const weight = e.target[1].value
+    console.log(height, weight);
+    calculateBmi(height, weight)
+    
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="form">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="height">Enter Your Height (cm): </label>
+              <input type="number" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="weight">Enter Your Weight (kg): </label>
+              <input type="number" />
+            </div>
+            <button type="submit">Send</button>
+          </form>
+          {rate &&
+            <div className="result">  
+              <p>{bmi}</p>
+              <p>{rate}</p>
+            </div>
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
